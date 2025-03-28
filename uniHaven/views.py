5eeeedc2ac2546f828622fdfe22a2dbc65ab9900
Ada-Au <adaau0209@gmail.com>
2025-03-28 12:04:51 +0800

    added accommodation view

--- uniHaven/views.py
+++ uniHaven/views.py
@@ -1,3 +1,12 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from uniHaven.models import Accommodation
 
-# Create your views here.
+def hello(request):
+    return HttpResponse('Hello, user!') 
+
+def accommodation_list(request):
+    context = {
+        "accommodations": Accommodation.objects.all()
+    }
+    return render(request, 'accommodation_list.html', context=context)
