a3d90c450978252448580319ad6c33f89e066435
Ada-Au <adaau0209@gmail.com>
2025-03-28 09:27:10 +0800

    added models

--- uniHaven/models.py
+++ uniHaven/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):  # For specialist, member
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    email = models.EmailField()
+    password = models.CharField(max_length=100)
+
+class PropertyOwner(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    contact = models.CharField(max_length=50) # email? phone number?
+
+class Accommodation(models.Model):
+    id = models.AutoField(primary_key=True)
+    type = models.CharField(max_length=50)
+    availablePeriodStart = models.DateField()
+    availablePeriodEnd = models.DateField()
+    bed = models.IntegerField()
+    bedroom = models.IntegerField()
+    price = models.FloatField()
+    address = models.CharField(max_length=100)
+    isAvailable = models.BooleanField(default=True)
+    owner = models.ForeignKey(PropertyOwner, on_delete=models.CASCADE)
